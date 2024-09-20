5d6d07e1d6088a47ccb28f2040a3275948f5c13f
Pleang1 <pleang1@hotmail.com>
2024-09-21 00:55:14 +0700

    update

--- rickroll/src/App.js
+++ rickroll/src/App.js
@@ -9,11 +9,12 @@ function App() {
     <div style={{ textAlign: '-webkit-center', marginTop: '2%' }}>
       {play === false && <button onClick={() => setPlay(true)} style={{ position: 'absolute', top: '50%', left: '50%' }}>Start</button>}
       <div style={{ display: play === true ? 'block' : 'none' }}>
-        <ReactPlayer url='https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        <ReactPlayer url='https://youtu.be/fn1PVDDo2lE?si=UUxjKa_VQbqUauhh&t=72'
           // style={{ margin: 'auto' }}
           width={1366}
           height={768}
           playing={play}
+          controls={true}
         />
       </div>
     </div>
